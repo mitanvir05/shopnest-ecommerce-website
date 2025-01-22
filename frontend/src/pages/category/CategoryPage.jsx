0cdab381413b2937c58f14d7bd55085fa5404a6d
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-01-22 12:17:27 +0600

    category added

---
+++ frontend/src/pages/category/CategoryPage.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import products from "../../data/products.json";
+import ProductCards from "../shop/ProductCards";
+const CategoryPage = () => {
+  const { categoryName } = useParams();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    const filtered = products.filter(
+      (product) => product.category === categoryName.toLowerCase()
+    );
+    setFilteredProducts(filtered);
+  }, [categoryName]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  })
+
+  return (
+    <>
+      <section className="section__container bg-primary-light">
+        <h2 className="section__header ">
+          Products in {categoryName.toUpperCase()}
+        </h2>
+        <p className="section__subheader">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus,
+          magni!
+        </p>
+        
+      </section>
+      {/* Product Cards */}
+      <div className="section__container">
+        <ProductCards products={filteredProducts}/>
+      </div>
+    </>
+  );
+};
+
+export default CategoryPage;
--- frontend/src/routers/routers.jsx
+++ frontend/src/routers/routers.jsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
+import CategoryPage from "../pages/category/CategoryPage";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[{
-      path: "/",
-      element: <Home></Home>,
-    }]
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/categories/:categoryName",
+        element: <CategoryPage/>,
+      },
+    ],
   },
 ]);
 export default router;
