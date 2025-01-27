57229729d67bf2ec6069f3006297722b2cee2fc8
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-01-27 13:13:42 +0600

    cart modal

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,10 +3,15 @@ import { IoIosSearch } from "react-icons/io";
 import { FaShoppingBag } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import CartModal from "../pages/shop/CartModal";
 
 const Navbar = () => {
-  const products= useSelector((state)=>state.cart.products)
-  console.log(products)
+  const products = useSelector((state) => state.cart.products);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const handleCartToggle = () => {
+    setIsCartOpen(!isCartOpen);
+  };
   return (
     <header className="fixed-nav-bar w-nav">
       <nav className="max-w-screen-2xl mx-auto px-4 flex justify-between items-center">
@@ -36,11 +41,11 @@ const Navbar = () => {
             </Link>
           </span>
           <span className="hover:text-primary">
-            <button>
+            <button onClick={handleCartToggle}>
               <FaShoppingBag />
             </button>
             <sup className="text-xs inline-block px-1 text-white rounded-full bg-primary text-center">
-             {products.length}
+              {products.length}
             </sup>
           </span>
           <span>
@@ -50,6 +55,8 @@ const Navbar = () => {
           </span>
         </div>
       </nav>
+
+      {isCartOpen && <CartModal products={products} isOpen={isCartOpen} onClose={handleCartToggle}/>}
     </header>
   );
 };
