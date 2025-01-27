5bb200fae5a79047b5d7dc1fd975e2944cd6ccc5
Md. Muhaimin Islam Tanvir <tanvir.cse05@gmail.com>
2025-01-27 13:37:24 +0600

    cart summary

--- frontend/src/pages/shop/OrderSummary.jsx
+++ frontend/src/pages/shop/OrderSummary.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { MdDelete } from "react-icons/md";
+import { FaMoneyCheckDollar } from "react-icons/fa6";
 
 const OrderSummary = () => {
+  const products = useSelector((store) => store.cart.products);
+  const { tax, taxRate, totalPrice, grandTotal, selectedItems } = useSelector(
+    (store) => store.cart
+  );
   return (
-    <div>
-      Order Summary
+    <div className="bg-primary-light mt-5 rounded text-base">
+      <div className="px-6 py-4 space-y-5">
+        <h2 className="text-xl text-text-dark"> Order Summary</h2>
+        <p className="text-text-dark">Selected Items : {selectedItems}</p>
+        <p>Total Price : ${totalPrice.toFixed(2)}</p>
+        <p>
+          Tax ({taxRate * 100}%) : ${tax.toFixed(2)}
+        </p>
+        <h3 className="font-bold">GrandTotal : ${grandTotal.toFixed(2)}</h3>
+        <div className="px-4 mb-6 ">
+          <button className="bg-red-500 px-3 py-1.5 text-white mt-2 rounded-md flex justify-between items-center mb-4">
+            <span className="mr-2">Clear Cart </span> <MdDelete size={18} />
+          </button>
+          <button className="bg-green-700 px-3 py-1.5 text-white mt-2 rounded-md flex justify-between items-center mb-4">
+            <span className="mr-2">Proceed CheckOut</span>
+            <FaMoneyCheckDollar size={18} />
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrderSummary
+export default OrderSummary;
